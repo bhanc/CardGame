faa5b745d9bf848505af96a8f76302f482779a16
Bartosz Hanc <bartosz.hanc@o2.pl>
2017-02-24 01:21:47 +0100

    Basic application

--- src/Card.java
+++ src/Card.java
@@ -2,4 +2,20 @@
  * Created by Bartosz Hanc on 2017-02-24.
  */
 public class Card {
+    private int attack;
+    private int defens;
+
+    public Card(int attack, int defens){
+        this.attack = attack;
+        this.defens = defens;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getDefens() {
+        return defens;
+    }
+
 }
