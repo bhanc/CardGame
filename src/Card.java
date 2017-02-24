164b682e3d8ea436fccf3ceb5dbb8a5ef0b93b7b
Bartosz Hanc <bartosz.hanc@o2.pl>
2017-02-24 01:18:25 +0100

    Revert "Basic application"
    This reverts commit 23c84d94a0aefec3668c49780da55b2acc2d3cb2.

--- src/Card.java
+++ src/Card.java
@@ -2,20 +2,4 @@
  * Created by Bartosz Hanc on 2017-02-24.
  */
 public class Card {
-    private int attack;
-    private int defens;
-
-    public Card(int attack, int defens){
-        this.attack = attack;
-        this.defens = defens;
-    }
-
-    public int getAttack() {
-        return attack;
-    }
-
-    public int getDefens() {
-        return defens;
-    }
-
 }
