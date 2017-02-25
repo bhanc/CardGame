eaedff01df24f0d23a5467d5b328dc4868eff538
Bartosz Hanc <bartosz.hanc@o2.pl>
2017-02-25 17:22:45 +0100

    Add generator cards and decks

--- src/Card.java
+++ src/Card.java
@@ -1,9 +1,19 @@
+import java.util.Random;
+
 /**
  * Created by Bartosz Hanc on 2017-02-24.
  */
 public class Card {
+    private String name;
     private int attack;
     private int defens;
+    private int random;
+    private Random generator = new Random();
+
+    public Card(String name){
+        this.name = name;
+        generateCard();
+    }
 
     public Card(int attack, int defens){
         this.attack = attack;
@@ -18,4 +28,19 @@ public class Card {
         return defens;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void generateCard(){
+        random = generator.nextInt(10) + 1;
+        attack = random;
+        random = generator.nextInt(10) + 1;
+        defens = random;
+    }
+
+    @Override
+    public String toString(){
+        return "Nazwa: " + name + "\nAtak: " + attack + "\nObrona: " + defens;
+    }
 }
